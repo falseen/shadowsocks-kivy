151f9cd43d1433441d64f28a5e82eda57e9e7239
Falseen <hurxyill@163.com>
2017-01-19 00:26:49 +0800

    add shadowsocks libeay32.dll
    and fix ss-local.py

--- kivy/ss-local.py
+++ kivy/ss-local.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import time,sys,os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
-from shadowsocks.shadowsocks import local
-local.main()
+sys.path.append('..')
+from shadowsocks import local
+
+def main():
+    local.main()
 
 
+if __name__ == '__main__':
+    main()
