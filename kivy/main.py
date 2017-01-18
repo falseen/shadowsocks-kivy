585bf0786714bfbd90668ee318dbb86bc6ded0e2
Falseen <hurxyill@163.com>
2017-01-18 23:55:38 +0800

    add kivy first

---
+++ kivy/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+from time import strftime
+
+
+class ShadowsocksApp(App):
+    sw_started = False
+    sw_seconds = 0
+
+    def on_start(self):
+        pass
+
+    def update(self, nap):
+        pass
+        
+    def start_stop(self):
+        self.root.ids.start_stop.text = 'Start' if self.sw_started else 'Stop'
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+
+        self.sw_seconds = 0
+
+if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex('#45818e')
+    LabelBase.register(name='Roboto',
+                       fn_regular='Roboto-Thin.ttf',
+                       fn_bold='Roboto-Medium.ttf')
+    LabelBase.register(name='simsun',
+                   fn_regular='simsun.ttc')
+              
+    ShadowsocksApp().run()
---
+++ kivy/ss-local.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import time,sys,os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+from shadowsocks.shadowsocks import local
+local.main()
+
+
