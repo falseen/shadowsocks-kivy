7d86caaa5c6551bbf8f31c799ae3afcfa316fb52
Falseen <hurxyill@163.com>
2017-02-08 13:59:48 +0800

    add vscode to .gitignore and fix another code

--- kivy/main.py
+++ kivy/main.py
@@ -9,25 +9,26 @@ from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from time import strftime
 import time
-
+from shadowsocks import local
 import ss_local
 import threading
+from multiprocessing import Process
+
 
 class ShadowsocksApp(App):
     sw_started = False
     sw_seconds = 0
 
-    
     def on_start(self):
         pass
 
     def update(self, nap):
         pass
-        
+
     def start_stop(self):
         self.root.ids.start_stop.text = 'Start' if self.sw_started else 'Stop'
         self.sw_started = not self.sw_started
-        threading.Thread(target=ss_local.main).start()
+        threading.Thread(target=local.main).start()
 
     def reset(self):
         if self.sw_started:
@@ -42,6 +43,6 @@ if __name__ == '__main__':
                        fn_regular='Roboto-Thin.ttf',
                        fn_bold='Roboto-Medium.ttf')
     LabelBase.register(name='simsun',
-                   fn_regular='simsun.ttc')
-              
+                       fn_regular='simsun.ttc')
+
     ShadowsocksApp().run()
